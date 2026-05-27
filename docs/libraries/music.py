f0f1f102dadcefc3c17354150367649da35d5391
0xda157 <da157@voidq.com>
2026-05-27 10:56:22 -0700

    libraries/music: add typing for functions

--- docs/libraries/music.py
+++ docs/libraries/music.py
@@ -12,11 +12,16 @@ import board, music
 music.playNotes(board.GP22, [('C',3,0.1), ('C',4,0.1), ('C',5,0.1)])
 """
 
-import pwmio, time
+import pwmio, microcontroller, time
 
 noteLetters = {'C':-9, 'C#':-8, 'Db':-8, 'D':-7, 'D#':-6, 'Eb':-6, 'E':-5, 'F':-4, 'F#':-3, 'Gb':-3, 'G':-2, 'G#':-1, 'Ab':-1, 'A':0, 'A#':1, 'Bb':1, 'B':2}
 
-def playTone(buzzerPin, frequency, duration=0.25, volume=100):
+def playTone(
+    buzzerPin: microcontroller.Pin,
+    frequency: int,
+    duration: int | float = 0.25,
+    volume: int | float = 100,
+):
     """
     plays the tone with the given frequency (in hertz), duration (in seconds), and volume (between 0..100)
     uses the buzzer attached to the given GPIO pin
@@ -26,7 +31,12 @@ def playTone(buzzerPin, frequency, duration=0.25, volume=100):
     pwm.deinit()
 
 
-def playTones(buzzerPin, frequencies, duration=0.25, volume=100):
+def playTones(
+    buzzerPin: microcontroller.Pin,
+    frequencies: list[int],
+    duration: int | float = 0.25,
+    volume: int | float = 100,
+):
     """
     plays the sequence of tones from the given list of frequencies (in hertz)
       using the given duration (in seconds), and volume (between 0..100)
@@ -39,7 +49,13 @@ def playTones(buzzerPin, frequencies, duration=0.25, volume=100):
     pwm.deinit()
 
 
-def playNote(buzzerPin, letter, octave=4, duration=0.25, volume=100):
+def playNote(
+    buzzerPin: microcontroller.Pin,
+    letter: str,
+    octave: int = 4,
+    duration: int | float = 0.25,
+    volume: int | float = 100,
+):
     """
     plays the note with the given letter (like 'A' or 'A#' or 'Ab')
       using the given octave (usually in the range 1..9), duration (in seconds), and volume (between 0..100)
@@ -51,7 +67,11 @@ def playNote(buzzerPin, letter, octave=4, duration=0.25, volume=100):
     pwm.deinit()
 
 
-def playNotes(buzzerPin, notes, volume = 100):
+def playNotes(
+    buzzerPin: microcontroller.Pin,
+    notes: list[tuple[str, int, int | float]],
+    volume: int = 100,
+):
     """
     plays the notes from the given notes list
        each note is a tuple wigh (letter, octave, duration)
@@ -67,7 +87,7 @@ def playNotes(buzzerPin, notes, volume = 100):
     pwm.deinit()
 
 
-def computeFrequency(letter, octave):
+def computeFrequency(letter: str, octave: int) -> int:
     """
     given a note letter (like 'A' or 'A#' or 'Ab') and an octave number (usually in the range 1..9)
     returns the frequency (in hertz) of that note
