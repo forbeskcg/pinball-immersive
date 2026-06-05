b75fa04d5e5b9483e2439d0b52295fcc59d05806
0xda157 <da157@voidq.com>
2026-06-05 15:03:00 -0700

    add a bunch more sounds

--- desktop/src/__main__.py
+++ desktop/src/__main__.py
@@ -5,7 +5,14 @@ import lib.graphics as graphics
 
 
 def startWorld(world):
-    world.sounds.winner_bell = graphics.loadSound("./sounds/winner bell.mp3")
+    world.sounds = {
+        "winner_bell": graphics.loadMusic("./desktop/sounds/winner_bell.mp3"),
+        "winning_slots": graphics.loadMusic("./desktop/sounds/winning_slots.mp3"),
+        "slots": graphics.loadMusic("./desktop/sounds/slots.mp3"),
+        "roulette_spin": graphics.loadMusic("./desktop/sounds/roulette_spin.mp3"),
+        "kids_cheering": graphics.loadMusic("./desktop/sounds/kids_cheering.mp3"),
+        "lose_tone": graphics.loadMusic("./desktop/sounds/lose_tone.mp3"),
+    }
 
 
 def updateWorld(world):
@@ -17,8 +24,9 @@ def updateWorld(world):
     print(f"task: {task}")
 
     if task[0] == "music":
+        # DO NOT remove or change existing sounds
         if task[1] == 1:
-            world.sounds.winner_bell.play()
+            graphics.playMusic(world.sounds["winner_bell"])
 
 
 if __name__ == "__main__":
@@ -28,4 +36,5 @@ if __name__ == "__main__":
     link.daemon = True
     link.start()
 
+    graphics.makeGraphicsWindow(1920, 1080)
     graphics.runGraphics(startWorld, updateWorld, lambda world: ...)
