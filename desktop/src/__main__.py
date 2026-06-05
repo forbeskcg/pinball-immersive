810116cf28944b8752fea14c501232d4bb9a82a1
0xda157 <da157@voidq.com>
2026-06-05 14:26:31 -0700

    fix graphics stuff

--- desktop/src/__main__.py
+++ desktop/src/__main__.py
@@ -4,32 +4,28 @@ import queue
 import lib.graphics as graphics
 
 
-def hello_world() -> None:
-    print("Hello World!")
+def startWorld(world):
+    world.sounds.winner_bell = graphics.loadSound("./sounds/winner bell.mp3")
 
 
-def play_sound(sound: int) -> None:
-    if sound == 1:
-        winner_bell = graphics.loadSound("./sounds/winner bell.mp3")
-        winner_bell.play()
+def updateWorld(world):
+    task = queue.get()
+
+    if task is None:
+        return
+
+    print(f"task: {task}")
+
+    if task[0] == "music":
+        if task[1] == 1:
+            world.sounds.winner_bell.play()
 
 
 if __name__ == "__main__":
     queue = queue.SimpleQueue()
 
-    play_sound(1)
-
     link = threading.Thread(target=lambda: datalink_server.run(queue))
     link.daemon = True
     link.start()
 
-    while True:
-        task = queue.get()
-
-        if task is None:
-            continue
-
-        print(f"task: {task}")
-
-        if task[0] == "music":
-            play_sound(task[1])
+    graphics.runGraphics(startWorld, updateWorld, lambda world: ...)
